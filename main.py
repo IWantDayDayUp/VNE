9e3e1b4f756b153a499b7a6b363d6348b911e5d3
IWantDayDayUp <1462959616@qq.com>
2024-07-18 20:50:56 +0800

    ✨ feat(main.py): main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import args
+import config
+
+
+def run(config: config):
+    print(f"\n{'-' * 20}    Start    {'-' * 20}\n")
+
+    print(f"\n{'-' * 20}    End    {'-' * 20}\n")
+    pass
+
+
+if __name__ == "__name__":
+    # 1. Get the command
+
+    # 2. Update config according to the given command
+
+    # 3. Generate Dataset, if already generated, load from file
+
+    # 4. Run with config
+    run(config=config)

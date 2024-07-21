74e05885eaf68c8f69596f8e3f36b5f200234dd2
IWantDayDayUp <1462959616@qq.com>
2024-07-21 09:35:54 +0800

    🐸 fun(data.py): Add new funs
    Get the directory of the dataset of p_net and v_net_sim from the setting

--- utils/data.py
+++ utils/data.py
@@ -40,3 +40,97 @@ def generate_data_with_distribution(size: int, distribution: str, dtype: str, **
         )
     return data.astype(dtype).tolist()
 
+
+def get_p_net_dataset_dir_from_setting(p_net_setting):
+    """Get the directory of the dataset of physical networks from the setting of the physical network simulation."""
+    p_net_dataset_dir = p_net_setting.get("save_dir")
+    n_attrs = [n_attr["name"] for n_attr in p_net_setting["node_attrs_setting"]]
+    e_attrs = [l_attr["name"] for l_attr in p_net_setting["link_attrs_setting"]]
+
+    if (
+        "file_path" in p_net_setting["topology"]
+        and p_net_setting["topology"]["file_path"] not in ["", None, "None"]
+        and os.path.exists(p_net_setting["topology"]["file_path"])
+    ):
+        p_net_name = (
+            f"{os.path.basename(p_net_setting['topology']['file_path']).split('.')[0]}"
+        )
+    else:
+        p_net_name = f"{p_net_setting['num_nodes']}-{p_net_setting['topology']['type']}_[{p_net_setting['topology']['wm_alpha']}-{p_net_setting['topology']['wm_beta']}]"
+    node_attrs_str = "-".join(
+        [
+            f'{n_attr_setting["name"]}_{get_parameters_string(get_distribution_parameters(n_attr_setting))}'
+            for n_attr_setting in p_net_setting["node_attrs_setting"]
+        ]
+    )
+    link_attrs_str = "-".join(
+        [
+            f'{e_attr_setting["name"]}_{get_parameters_string(get_distribution_parameters(e_attr_setting))}'
+            for e_attr_setting in p_net_setting["link_attrs_setting"]
+        ]
+    )
+
+    p_net_dataset_middir = p_net_name + "-" + node_attrs_str + "-" + link_attrs_str
+    # f"{n_attrs}-[{p_net_setting['node_attrs_setting'][0]['low']}-{p_net_setting['node_attrs_setting'][0]['high']}]-" + \
+    # f"{e_attrs}-[{p_net_setting['link_attrs_setting'][0]['low']}-{p_net_setting['link_attrs_setting'][0]['high']}]"
+    p_net_dataset_dir = os.path.join(p_net_dataset_dir, p_net_dataset_middir)
+
+    return p_net_dataset_dir
+
+
+def get_v_nets_dataset_dir_from_setting(v_sim_setting):
+    """Get the directory of the dataset of virtual networks from the setting of the virtual network simulation."""
+    v_nets_dataset_dir = v_sim_setting.get("save_dir")
+    # n_attrs = [n_attr['name'] for n_attr in v_sim_setting['node_attrs_setting']]
+    # e_attrs = [l_attr['name'] for l_attr in v_sim_setting['link_attrs_setting']]
+    node_attrs_str = "-".join(
+        [
+            f'{n_attr_setting["name"]}_{get_parameters_string(get_distribution_parameters(n_attr_setting))}'
+            for n_attr_setting in v_sim_setting["node_attrs_setting"]
+        ]
+    )
+    link_attrs_str = "-".join(
+        [
+            f'{e_attr_setting["name"]}_{get_parameters_string(get_distribution_parameters(e_attr_setting))}'
+            for e_attr_setting in v_sim_setting["link_attrs_setting"]
+        ]
+    )
+
+    v_nets_dataset_middir = (
+        f"{v_sim_setting['num_v_nets']}-[{v_sim_setting['v_net_size']['low']}-{v_sim_setting['v_net_size']['high']}]-"
+        + f"{v_sim_setting['topology']['type']}-{get_parameters_string(get_distribution_parameters(v_sim_setting['lifetime']))}-{v_sim_setting['arrival_rate']['lam']}-"
+        + node_attrs_str
+        + "-"
+        + link_attrs_str
+    )
+    # f"{n_attrs}-[{v_sim_setting['node_attrs_setting'][0]['low']}-{v_sim_setting['node_attrs_setting'][0]['high']}]-" + \
+    # f"{e_attrs}-[{v_sim_setting['link_attrs_setting'][0]['low']}-{v_sim_setting['link_attrs_setting'][0]['high']}]"
+    v_net_dataset_dir = os.path.join(v_nets_dataset_dir, v_nets_dataset_middir)
+    return v_net_dataset_dir
+
+
+def get_distribution_parameters(distribution_dict):
+    """Get the parameters of the distribution."""
+    distribution = distribution_dict.get("distribution", None)
+    if distribution is None:
+        return []
+    if distribution == "exponential":
+        parameters = [distribution_dict["scale"]]
+    elif distribution == "possion":
+        parameters = [distribution_dict["lam"]]
+    elif distribution == "uniform":
+        parameters = [distribution_dict["low"], distribution_dict["high"]]
+    elif distribution == "customized":
+        parameters = [distribution_dict["min"], distribution_dict["max"]]
+    return parameters
+
+
+def get_parameters_string(parameters):
+    """Get the string of the parameters."""
+    if len(parameters) == 0:
+        return "None"
+    elif len(parameters) == 1:
+        return str(parameters[0])
+    else:
+        str_parameters = [str(p) for p in parameters]
+        return f'[{"-".join(str_parameters)}]'

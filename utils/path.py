06fec16a3870417398b840db38a23cc39ee30ff5
IWantDayDayUp <1462959616@qq.com>
2024-07-21 09:34:36 +0800

    🐸 fun(path.py): Add new fun `path_to_links`
    Converts a given path to a list of tuples containing two elements each.

---
+++ utils/path.py
@@ -0,0 +1,15 @@
+import networkx as nx
+
+
+def path_to_links(path: list) -> list:
+    """
+    Converts a given path to a list of tuples containing two elements each.
+
+    Args:
+        path (list): A list of elements representing a path.
+
+    Returns:
+        list: A list of tuples, each tuple containing two elements from the given path.
+    """
+    assert len(path) > 1
+    return [(path[i], path[i + 1]) for i in range(len(path) - 1)]

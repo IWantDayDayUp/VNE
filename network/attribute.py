7e89f2ca075ef036541de6e4f55beea3b0c577ef
IWantDayDayUp <1462959616@qq.com>
2024-07-25 12:09:15 +0800

    🐞 fix(attribute.py): Can't get `net.number_of_links()`
    `net.number_of_links()` -> `net.number_of_edges()`

--- network/attribute.py
+++ network/attribute.py
@@ -8,8 +8,49 @@ sys.path.append(
     ".."
 )  # 等价于 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from utils.path import path_to_links
-from utils.data import generate_data_with_distribution
+# from utils.path import path_to_links
+# from utils.data import generate_data_with_distribution
+
+import numpy as np
+
+
+def generate_data_with_distribution(size: int, distribution: str, dtype: str, **kwargs):
+    """
+    Generate data with the given distribution and data type.
+
+    Args:
+        size (int): The size of the data.
+        distribution (str): The distribution of the data.
+        dtype (str): The data type of the data.
+        **kwargs: Keyword arguments to pass to the distribution generator.
+
+    Returns:
+        np.ndarray: The generated data.
+    """
+    assert distribution in ["uniform", "normal", "exponential", "possion"]
+    assert dtype in ["int", "float", "bool"]
+    if distribution == "normal":
+        loc, scale = kwargs.get("loc"), kwargs.get("scale")
+        data = np.random.normal(loc, scale, size)
+    elif distribution == "uniform":
+        low, high = kwargs.get("low"), kwargs.get("high")
+        if dtype == "int":
+            data = np.random.randint(low, high + 1, size)
+        elif dtype == "float":
+            data = np.random.uniform(low, high, size)
+    elif distribution == "exponential":
+        scale = kwargs.get("scale")
+        data = np.random.exponential(scale, size)
+    elif distribution == "possion":
+        lam = kwargs.get("lam")
+        if kwargs.get("reciprocal", False):
+            lam = 1 / lam
+        data = np.random.poisson(lam, size)
+    else:
+        raise NotImplementedError(
+            f"Generating {dtype} data following the {distribution} distribution is unsupporrted!"
+        )
+    return data.astype(dtype).tolist()
 
 
 class Attribute(object):
@@ -144,7 +185,7 @@ class Attribute(object):
         """
         assert self.generative
         # size = net.num_nodes if self.owner == "node" else net.num_links
-        size = net.number_of_nodes() if self.owner == "node" else net.number_of_links()
+        size = net.number_of_nodes() if self.owner == "node" else net.number_of_edges()
 
         if self.distribution == "uniform":
             kwargs = {"low": self.low, "high": self.high}
